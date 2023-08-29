8dbe8bdaf9fe82fffb9fb44c56806f4ffd3adf66
surajjayraman <suraj.jayraman@gmail.com>
2023-08-29 11:20:14 -0400

    Update search component

--- src/App.js
+++ src/App.js
@@ -61,11 +61,11 @@ function ProductTable({ products }) {
 function SearchBar() {
   return (
     <form>
-      <input type="text" placeholder="Search..." />
+      
       <label>
-        <input type="checkbox" />
+        
         {' '}
-        Only show products in stock
+        
       </label>
     </form>
   );
