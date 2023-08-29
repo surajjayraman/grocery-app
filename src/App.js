5a57ad477ce78d8cdb9982a4f724d0693c2c4d7b
surajjayraman <suraj.jayraman@gmail.com>
2023-08-29 13:03:48 -0400

    Update product stock status

--- src/App.js
+++ src/App.js
@@ -83,9 +83,9 @@ function FilterableProductTable({ products }) {
 const PRODUCTS = [
   {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
   {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Fruits", price: "$2", stocked: true, name: "Passionfruit"},
   {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$4", stocked: true, name: "Pumpkin"},
   {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
 ];
 
