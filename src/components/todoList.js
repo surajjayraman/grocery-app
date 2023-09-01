25decfca052a967d54a51513aca7c35b2dfccb51
surajjayraman <suraj.jayraman@gmail.com>
2023-09-01 16:16:55 -0400

    Add double curlies example

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -13,6 +13,14 @@ export default function TodoList() {
         <>
             <h1>{name}'s To Do List</h1>
             <h3>To Do List for {formatDate(today)}</h3>
+            <ul style={{
+                        backgroundColor: 'black',
+                        color: 'pink'
+                        }}>
+                        <li>Improve the videophone</li>
+                        <li>Prepare aeronautics lectures</li>
+                        <li>Work on the alcohol-fuelled engine</li>
+            </ul>
         </>
     );
 }
