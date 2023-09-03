bca27bcf443e1317ec81c15aad2fd200d792dace
surajjayraman <suraj.jayraman@gmail.com>
2023-09-03 14:43:23 -0400

    Add conditional rendering of people data

--- src/components/list.js
+++ src/components/list.js
@@ -1,20 +1,45 @@
-const people = [
+import { people } from './data';
+import { getImageUrl } from "./utils";
+
+/*const people = [
     'Creola Katherine Johnson: mathematician',
     'Mario José Molina-Pasquel Henríquez: chemist',
     'Mohammad Abdus Salam: physicist',
     'Percy Lavon Julian: chemist',
     'Subrahmanyan Chandrasekhar: astrophysicist'
-  ];
+  ];*/
   
   export default function List() {
+    const chemists = people.filter((person) => {
+        return person.profession === 'chemist';
+    });
+
     const listItems = people.map((person) => {
-       return <li>{person}</li>
+       return <li>{person.name}</li>
+    });
+
+    const chemistList = chemists.map((chemist) => {
+        return (
+        <>
+            <img
+            src={getImageUrl(chemist)}
+            alt={chemist.name}
+            />
+            <p>
+                <b>{chemist.name}:</b>
+                {' ' + chemist.profession + ' '}
+                known for {chemist.accomplishment}
+            </p>
+        </> )
+
     });
 
     return (
         <>
         <h2>Rendering Lists</h2>
         <ul>{listItems}</ul>
+        <h2>All chemists</h2>
+        <ul>{chemistList}</ul>
         </>
         
     );
