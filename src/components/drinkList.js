d7986aa96440bfa139ccfd77800357e7fcef2ddb
surajjayraman <suraj.jayraman@gmail.com>
2023-09-03 07:37:26 -0400

    Update DrinkList component

--- src/components/drinkList.js
+++ src/components/drinkList.js
@@ -1,3 +1,16 @@
+const drinks = {
+    tea: {
+      part: 'leaf',
+      caffeine: '15–70 mg/cup',
+      age: '4,000+ years'
+    },
+    coffee: {
+      part: 'bean',
+      caffeine: '80–185 mg/cup',
+      age: '1,000+ years'
+    }
+  };
+  
 function Drink({ name }) {
     return (
       <section>
