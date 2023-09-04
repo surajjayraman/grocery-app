220a9a0100a7459d54414bbe3870f5d428731e15
surajjayraman <suraj.jayraman@gmail.com>
2023-09-04 09:14:51 -0400

    Update RecipeList rendering

--- src/components/recipeList.js
+++ src/components/recipeList.js
@@ -1,10 +1,31 @@
 import { recipes} from './data';
-import { getImageUrl } from "./utils";
 
 const RecipeList = () => {
-    return <div>
-        <h2>Available Recipes</h2>
-    </div>
+    const recipeItems = recipes.map((item) => {
+        return (
+            <>
+                <div key={item.id}>
+                    <h2>{item.name}</h2>
+                    <ul>
+                        {item.ingredients.map((ingredient) => {
+                            return (
+                                <li key={ingredient}>
+                                        {ingredient}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </>
+
+        );
+    });
+    return (
+        <div>
+            <h1>Available Recipes</h1>
+                {recipeItems}
+        </div>
+    )
 }
 
 export default RecipeList;
