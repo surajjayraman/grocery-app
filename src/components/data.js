76e7d568084a7b8500b0a51f2b12776c9d5ab399
surajjayraman <suraj.jayraman@gmail.com>
2023-09-04 08:16:39 -0400

    Add recipe data

--- src/components/data.js
+++ src/components/data.js
@@ -1,3 +1,4 @@
+// people data
 export const people = [{
     id: 0,
     name: 'Creola Katherine Johnson',
@@ -29,4 +30,17 @@ export const people = [{
     accomplishment: 'white dwarf star mass calculations',
     imageId: 'lrWQx8l'
   }];
-  
+  // recipe data
+  export const recipes = [{
+    id: 1,
+    name: 'Greek Salad',
+    ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta']
+  }, {
+    id: 2,
+    name: 'Hawaiian Pizza',
+    ingredients: ['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple']
+  }, {
+    id: 3,
+    name: 'Hummus',
+    ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini']
+  }];
