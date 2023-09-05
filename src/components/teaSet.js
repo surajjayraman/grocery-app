d1ffee179ba4dfc25e60062330c94a0b75d2086c
surajjayraman <suraj.jayraman@gmail.com>
2023-09-05 18:29:00 -0400

    Add DrinkRecipe component

--- src/components/teaSet.js
+++ src/components/teaSet.js
@@ -1,14 +1,30 @@
-function Cup({guest}) {
-    return <h2>Tea cup for guest #{guest}</h2>;
+const Cup = ({guest}) => {
+    return <p>Tea cup for guest #{guest}</p>;
 }
 
+const DrinkRecipe = ({ drinkers }) => {
+    return (
+      <ol>
+        <li>Boil {drinkers} cups of water.</li>
+        <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.</li>
+        <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
+      </ol>
+    );
+  }
 export default function TeaSet() {
   return (
     <>
-      <h3>Welcome to our kitty party</h3>  
+      <h1>Welcome to our kitty party</h1>
       <Cup guest={1} />
       <Cup guest={2}/>
       <Cup guest={3}/>
+      <section>
+        <h1>Spiced Chai Recipe</h1>
+        <p>For two</p>
+        <DrinkRecipe drinkers={2}/>
+        <p>For a gathering</p>
+        <DrinkRecipe drinkers={4}/>
+      </section>
     </>
   );
 }
