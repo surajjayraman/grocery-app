ff300755000b13c4be17cc62b057fb988e60f0cd
surajjayraman <suraj.jayraman@gmail.com>
2023-09-05 12:02:34 -0400

    Update Poem styles

--- src/components/poem.js
+++ src/components/poem.js
@@ -1,4 +1,4 @@
-const poem = {
+/*const poem = {
     lines: [
       'I write, erase, rewrite',
       'Erase again, and then',
@@ -19,4 +19,37 @@ const poem = {
             </article>
         </>
     );
+  }*/
+
+  const poem = {
+    lines: [
+      'I write, erase, rewrite',
+      'Erase again, and then',
+      'A poppy blooms.'
+    ]
+  };
+  
+  export default function Poem() {
+    let output = [];
+  
+    // Fill the output array
+    poem.lines.forEach((line, i) => {
+      output.push(
+        <hr key={i + '-separator'} />
+      );
+      output.push(
+        <p key={i + '-text'}>
+          {line}
+        </p>
+      );
+    });
+    // Remove the first <hr />
+    output.shift();
+  
+    return (
+      <article>
+        {output}
+      </article>
+    );
   }
+  
