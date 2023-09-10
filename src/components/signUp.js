1c1641b594304177d506ccb12ff2d0e0f029af61
surajjayraman <suraj.jayraman@gmail.com>
2023-09-09 22:41:43 -0400

    Update SignUp component

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -2,14 +2,19 @@ const SignUp = () => {
     return (
         <>
             <h1>Preventing Default Behaviour</h1>
-            <form onSubmit={() => alert('Submitting')} >
+            <form onSubmit={
+                    e => {
+                        e.preventDefault();
+                        alert('Submitting');
+                    }
+                }
+             >
             <input />
             <button>
                 Send
             </button>
             </form>
         </>
-        
     )
 }
 
