de052b809d57164f79964fe8a138d7b4c8525c06
surajjayraman <suraj.jayraman@gmail.com>
2023-09-13 13:54:10 -0400

    Update FormStateDemo component

--- src/components/formStateDemo.js
+++ src/components/formStateDemo.js
@@ -4,16 +4,32 @@ const FormStateDemo = () => {
     const [isSent,setIsSent]= useState(false);
     const [message, setMessage] = useState('Hi!');
 
+    if (isSent){
+        return (
+            <h1>Your message is on its way!</h1>
+        )
+    }
+    const handleTextChange = (e) => {
+        setMessage(e.target.value);
+    }
+
 
     return (
         <>
         <form>
             <textarea
                 placeholder="Message"
-                value={message}>    
+                value={message}
+                onChange={handleTextChange}>    
             </textarea>
             <br />
-            <button type="submit">
+            <button type="submit"
+                onClick={ 
+                    (e) => {
+                        e.preventDefault();
+                        setIsSent(true);
+                    }
+                }>
                 Send
             </button>
         </form>
