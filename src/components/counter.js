81c005be45fa33ce03d5446f6129e4fc5e7bb841
surajjayraman <suraj.jayraman@gmail.com>
2023-09-13 21:53:17 -0400

    Update Counter component

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,6 +2,15 @@ import { useState } from 'react';
 
 export default function Counter() {
   const [number, setNumber] = useState(0);
+  const [to, setTo] = useState('Alice');
+  const [message, setMessage] = useState('Hello');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`);
+    }, 5000);
+  }
 
   return (
     <>
@@ -13,6 +22,23 @@ export default function Counter() {
           alert(number);
         }, 3000);
       }}>+5</button>
+      <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+        <select
+          value={to}
+          onChange={e => setTo(e.target.value)}>
+          <option value="Alice">Alice</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </label>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
     </>
   )
 }
