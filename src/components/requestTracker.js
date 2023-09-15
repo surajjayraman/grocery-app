ae2dbfeffe2d6d8f9b13e520e5111c6e20bbe1e2
surajjayraman <suraj.jayraman@gmail.com>
2023-09-15 09:34:06 -0400

    Update RequestTracker with Order status

--- src/components/requestTracker.js
+++ src/components/requestTracker.js
@@ -2,15 +2,27 @@ import { useState } from 'react';
 
 const RequestTracker = () => {
     const [order, setOrder] = useState(0);
+    const [pending,setPending] = useState(0);
 
     const handleOrder = () => {
         setOrder(order => order + 1);
+        setPending(pending => pending + 1);
     }
     return (
         <>
             <h1>Track your orders!</h1>
-            <p style={{color: 'purple'}}>Order Completed: {order}</p>
-            <button onClick={handleOrder}>
+            <p style={{color: 'Orange'}}>Order Pending: {pending}</p>
+            <p style={{color: 'purple'}}>Order Processing: {order}</p>
+            <button onClick={ () => {
+                handleOrder();
+                setTimeout(() => {
+                    //alert('processing order');
+                    setPending(pending => pending - 1);
+                  }, 3000);
+
+                }
+
+            }>
                 Buy
             </button>
         </>
