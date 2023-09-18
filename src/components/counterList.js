601693a192a92fddfb29a8e75efdbfe9658c5e91
surajjayraman <suraj.jayraman@gmail.com>
2023-09-18 16:27:33 -0400

    Update CounterList component

--- src/components/counterList.js
+++ src/components/counterList.js
@@ -1,18 +1,34 @@
 
-
+import { useState } from 'react';
 
 export default function CounterList() {
-  
-  
+  const [counter, setCounter] = useState([0,0,0]);
+
+  const handleClick = (index) => {
+    //const newCounter = [...counter];
+    //newCounter[index] += 1;
+    const newCounter = counter.map((c,i) => {
+      if (i === index){
+        return c + 1;
+      }
+      return c;
+    });
+    setCounter([
+      ...newCounter
+    ]);
+  }
+
 
   return (
     <>
     <p style={{color:'darkcyan'}}>Replacing items in an array - Demo</p>
     <ul>
-      <li>
-        <button>+1</button>
+      {counter.map((c,i) => {
+        return <li key={i}>
+          {c}{' '}
+        <button onClick={() => handleClick(i)}>+1</button>
       </li>
-     
+      })}
     </ul>
     </>
   );
