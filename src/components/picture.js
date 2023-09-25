47c385cfe5edbde3b7c5f7d54130f269f954e6ae
surajjayraman <suraj.jayraman@gmail.com>
2023-09-25 16:07:23 -0400

    Refactor Picture component with dynamic css

--- src/components/picture.js
+++ src/components/picture.js
@@ -1,21 +1,32 @@
 import { useState } from 'react';
 
 export default function Picture() {
-    const [style, setStyle] = useState('');
-    const [status, setStatus] = useState(false);
-    const handleStyle = () => {
-        status ? setStyle('background background--active') : setStyle('background picture--active');
-        setStatus(!status);
-    }
+
+    const [isActive, setIsActive] = useState(false);
+
+    let backgroundClassName = 'background';
+    let pictureClassName = 'picture';
+
+    isActive ? pictureClassName += ' picture--active'
+            : backgroundClassName += ' background--active';
+
+
     return (
         <>
         <p style={{color:'darkorange'}}>Add and remove a CSS class </p>
-        <div className={style} onClick = {handleStyle}>
+        <div className={backgroundClassName} onClick = {() => {
+            setIsActive(false);
+
+        }}>
             <img
-            className="picture"
+            className={pictureClassName}
             alt="Rainbow houses in Kampung Pelangi, Indonesia"
             src="https://i.imgur.com/5qwVYb1.jpeg"
-            
+            onClick={e => {
+                e.stopPropagation();
+                setIsActive(true);
+
+            }}
             />
         </div>
         </>
