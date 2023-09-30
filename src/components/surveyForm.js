6eb2990fad9260637e3762cfd9f3b8144b871d79
surajjayraman <suraj.jayraman@gmail.com>
2023-09-30 19:34:39 -0400

    Update SurveyForm component with conditional states

--- src/components/surveyForm.js
+++ src/components/surveyForm.js
@@ -2,28 +2,42 @@ import { useState } from 'react';
 
 const SurveyForm = () => {
     const [answer, setAnswer] = useState('Excellent vacation!')
-    const [isEdit, setIsEdit] = useState(false);
+    const [isSent, setIsSent] = useState(false);
+    const [isSending, setIsSending] = useState(false);
 
-    if (isEdit) {
+    if (isSent) {
         return <p>Thank You for the feedback!</p>
     }
 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        setIsEdit(true);
+        setIsSending(true);
+        await sendMessage();
+        setIsSent(!isSent);
     }
+
+    // Pretend to send a message.
+   const sendMessage = () => {
+    return new Promise(resolve => {
+      setTimeout(resolve, 2000);
+    });
+  }
 
     return (
         <>
         <h1 style={{color:'darkorchid'}}>Choosing the State Structure</h1>
         <form onSubmit={handleSubmit}>
             <p>How was your stay at The Prancing Pony?</p>
-            <textarea value={answer} onChange={e => setAnswer(e.target.value)}>
+            <textarea value={answer}
+                disabled={isSending}
+                onChange={e => setAnswer(e.target.value)}>
             </textarea>
-            <button type='submit'>
+            <button type='submit'
+                disabled={isSending}>
                 Send
             </button>
+            {isSending && <p>Sending...{answer}</p>}
         </form>
 
         </>
