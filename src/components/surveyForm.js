5021c640ab72ef8cc6d74d65b7d17ec59d3845f3
surajjayraman <suraj.jayraman@gmail.com>
2023-09-30 17:22:01 -0400

    Update SurveyForm component with submit handler

--- src/components/surveyForm.js
+++ src/components/surveyForm.js
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 
 const SurveyForm = () => {
+    const [answer, setAnswer] = useState('Excellent vacation!')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
     return (
         <>
         <h1 style={{color:'darkorchid'}}>Choosing the State Structure</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <p>How was your stay at The Prancing Pony?</p>
-            <textarea>
-
+            <textarea value={answer} onChange={e => setAnswer(e.target.value)}>
             </textarea>
-            <button>
+            <button type='submit'>
                 Send
             </button>
         </form>
