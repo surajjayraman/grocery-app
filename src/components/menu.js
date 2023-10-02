0d33869b80bc8432ae98e06391f6eb24a2abc5a6
surajjayraman <suraj.jayraman@gmail.com>
2023-10-01 21:11:45 -0400

    Update component Menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -10,23 +10,39 @@ export default function Menu() {
   const [items, setItems] = useState(initialItems);
   const [selectedItem, setSelectedItem] = useState(
     items[0]
-  );
+  )
+
+  const handleItemChange = (e, id) => {
+    const updatedItems = items.map(item => {
+        if (id === item.id){
+            return {
+                id: item.id,
+                title: e.target.value
+            };
+        }
+        return item;
+     } );
+     setItems(updatedItems);
+  }
+
 
   return (
     <>
-      <p style={{color: 'darkmagenta'}}>Air Canada Meal Plan</p>
+      <p style={{color: 'darkmagenta'}}>British Airway's Meal Plan</p>
       <h2>What's your travel snack?</h2>
-      <ul>
+       <ul>
         {items.map(item => (
-          <li key={item.id}>
-            {item.title}
+          <li>
+            <input key={item.id}
+            value={item.title}
+            onChange={e => handleItemChange(e, item.id)}/>
             {' '}
-            <button onClick={() => {
-              setSelectedItem(item);
-            }}>Choose</button>
-          </li>
+            <button onClick={() => setSelectedItem(item)}>Choose</button>
+            </li>
+
         ))}
-      </ul>
+        </ul>
+
       <p>You picked {selectedItem.title}.</p>
     </>
   );
