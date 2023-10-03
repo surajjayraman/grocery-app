c2c678c086ead26a172fb7d449e2ea5c8a95ddf3
surajjayraman <suraj.jayraman@gmail.com>
2023-10-03 16:45:43 -0400

    Refactor TravelPlan component with useImmer

--- src/components/travelPlan.js
+++ src/components/travelPlan.js
@@ -1,61 +1,31 @@
-import { useState } from 'react';
+import { useImmer } from 'use-immer';
 import { initialTravelPlan } from './places.js';
 
-function PlaceTree({ id, parentId, placesById, onComplete }) {
-    const place = placesById[id];
-    const childIds = place.childIds;
-    return (
-      <li>
-        {place.title}
-        <button onClick={() => {
-          onComplete(parentId, id);
-        }}>
-          Complete
-        </button>
-        {childIds.length > 0 &&
-          <ol>
-            {childIds.map(childId => (
-              <PlaceTree
-                key={childId}
-                id={childId}
-                parentId={id}
-                placesById={placesById}
-                onComplete={onComplete}
-              />
-            ))}
-          </ol>
-        }
-      </li>
-    );
-  }
-  
-
 export default function TravelPlan() {
-    const [plan, setPlan] = useState(initialTravelPlan);
-    const root = plan[0];
-    const planetIds = root.childIds;
-   
-    function handleComplete(parentId, childId) {
-        const parent = plan[parentId];
-        // Create a new version of the parent place
-        // that doesn't include this child ID.
-        const nextParent = {
-          ...parent,
-          childIds: parent.childIds
-            .filter(id => id !== childId)
-        };
-        // Update the root state object...
-        setPlan({
-          ...plan,
-          // ...so that it has the updated parent.
-          [parentId]: nextParent
-        });
-      }
+  const [plan, updatePlan] = useImmer(initialTravelPlan);
 
+  function handleComplete(parentId, childId) {
+    updatePlan(draft => {
+      // Remove from the parent place's child IDs.
+      const parent = draft[parentId];
+      parent.childIds = parent.childIds
+        .filter(id => id !== childId);
+
+      // Forget this place and all its subtree.
+      deleteAllChildren(childId);
+      function deleteAllChildren(id) {
+        const place = draft[id];
+        place.childIds.forEach(deleteAllChildren);
+        delete draft[id];
+      }
+    });
+  }
+
+  const root = plan[0];
+  const planetIds = root.childIds;
   return (
     <>
-      <h1 style={{color:'darkorchid'}}>My Travel Plans!!!</h1>
-      <p style={{color:'darkgreen'}}>Places to visit</p>
+      <h2>Places to visit</h2>
       <ol>
         {planetIds.map(id => (
           <PlaceTree
@@ -68,5 +38,33 @@ export default function TravelPlan() {
         ))}
       </ol>
     </>
+  );
+}
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+  const place = placesById[id];
+  const childIds = place.childIds;
+  return (
+    <li>
+      {place.title}
+      <button onClick={() => {
+        onComplete(parentId, id);
+      }}>
+        Complete
+      </button>
+      {childIds.length > 0 &&
+        <ol>
+          {childIds.map(childId => (
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
+            />
+          ))}
+        </ol>
+      }
+    </li>
   );
 }
