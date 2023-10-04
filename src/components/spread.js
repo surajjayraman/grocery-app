8f79027b509c99616afd51ba3948aa57348c4250
surajjayraman <suraj.jayraman@gmail.com>
2023-10-04 13:18:35 -0400

    Update spread operator demo

--- src/components/spread.js
+++ src/components/spread.js
@@ -1,8 +1,9 @@
 // What does the Spread operator do?
 // Ans. cannot stand on its own like other operators
+// shallow copy
 
 const array1 = [1,2,3];
 const array2 = [1,2,3];
 
-const array3 = [...array1];
+const array3 = [...array1,4,5,6];
 console.log(array3);
