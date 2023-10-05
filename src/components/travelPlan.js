d386db9ebdc881708b2cbae275db0373eb41c2b2
surajjayraman <suraj.jayraman@gmail.com>
2023-10-04 22:54:27 -0400

    Refactor TravelList component

--- src/components/travelPlan.js
+++ src/components/travelPlan.js
@@ -1,8 +1,6 @@
-import { useImmer } from 'use-immer';
-import { useState } from 'react'
-import { initialTravelPlan } from './places.js';
-import TravelList from './travelList.js';
+import { useState } from 'react';
 import AddItem from './addItem.js';
+import TravelList from './travelList.js';
 
 let nextId = 3;
 const initialItems = [
@@ -12,29 +10,14 @@ const initialItems = [
 ];
 
 export default function TravelPlan() {
-  const [plan, updatePlan] = useImmer(initialTravelPlan);
   const [items, setItems] = useState(initialItems);
-  const [total, setTotal] = useState(3);
-  const [packed, setPacked] = useState(1);
 
-  function handleComplete(parentId, childId) {
-    updatePlan(draft => {
-      // Remove from the parent place's child IDs.
-      const parent = draft[parentId];
-      parent.childIds = parent.childIds
-        .filter(id => id !== childId);
+  const total = items.length;
+  const packed = items
+    .filter(item => item.packed)
+    .length;
 
-      // Forget this place and all its subtree.
-      deleteAllChildren(childId);
-      function deleteAllChildren(id) {
-        const place = draft[id];
-        place.childIds.forEach(deleteAllChildren);
-        delete draft[id];
-      }
-    });
-  }
   function handleAddItem(title) {
-    setTotal(total => total + 1);
     setItems([
       ...items,
       {
@@ -44,12 +27,8 @@ export default function TravelPlan() {
       }
     ]);
   }
+
   function handleChangeItem(nextItem) {
-    if (nextItem.packed) {
-      setPacked(packed => packed + 1);
-    } else {
-      setPacked(packed => packed - 1);
-    }
     setItems(items.map(item => {
       if (item.id === nextItem.id) {
         return nextItem;
@@ -58,42 +37,20 @@ export default function TravelPlan() {
       }
     }));
   }
+
   function handleDeleteItem(itemId) {
-    setTotal(total => total - 1);
-    const updatedItems = items.map(item => {
-      if (item.id === itemId && item.packed){
-        setPacked(packed => packed - 1);
-      }
-      return item;
-    })
     setItems(
-      updatedItems.filter(item => item.id !== itemId)
+      items.filter(item => item.id !== itemId)
     );
   }
 
-
-
-  const root = plan[0];
-  const planetIds = root.childIds;
   return (
-    <>
-      <h2>Places to visit</h2>
-      <ol>
-        {planetIds.map(id => (
-          <PlaceTree
-            key={id}
-            id={id}
-            parentId={0}
-            placesById={plan}
-            onComplete={handleComplete}
-          />
-        ))}
-      </ol>
-      <p style={{color:'purple'}}>Welcome to my Travel Pack list!!!</p>
+    <> 
+      <p style={{color:'rebeccapurple'}}>My Travel kit!!</p> 
       <AddItem
         onAddItem={handleAddItem}
       />
-     <TravelList
+      <TravelList
         items={items}
         onChangeItem={handleChangeItem}
         onDeleteItem={handleDeleteItem}
@@ -101,35 +58,5 @@ export default function TravelPlan() {
       <hr />
       <b>{packed} out of {total} packed!</b>
     </>
-  );
-}
-
-function PlaceTree({ id, parentId, placesById, onComplete }) {
-  const place = placesById[id];
-  const childIds = place.childIds;
-  return (
-    
-    <li>
-      {place.title}
-      <button onClick={() => {
-        onComplete(parentId, id);
-      }}>
-        Complete
-      </button>
-      {childIds.length > 0 &&
-        <ol>
-          {childIds.map(childId => (
-            <PlaceTree
-              key={childId}
-              id={childId}
-              parentId={id}
-              placesById={placesById}
-              onComplete={onComplete}
-            />
-          ))}
-        </ol>
-      }
-    </li>
-    
   );
 }
