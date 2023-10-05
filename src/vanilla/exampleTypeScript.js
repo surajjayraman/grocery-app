f07bf3a641824381e0000404aca5fb7a2a612454
surajjayraman <suraj.jayraman@gmail.com>
2023-10-05 18:00:01 -0400

    Add TypeScript checks

---
+++ src/vanilla/exampleTypeScript.js
@@ -0,0 +1,6 @@
+var sport = 'football';
+var id = 5;
+id = 10;
+var firstName = 'Danny';
+console.log(firstName.toLowerCase());
+console.log(firstName);
---
+++ src/vanilla/exampleTypeScript.ts
@@ -0,0 +1,7 @@
+let sport: string = 'football';
+let id: number = 5;
+id = 10;
+
+let firstName = 'Danny';
+console.log(firstName.toLowerCase());
+console.log(firstName);
