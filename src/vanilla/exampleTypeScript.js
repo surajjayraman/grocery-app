9b032d87e8422f7c240b65454372129e4486093c
surajjayraman <suraj.jayraman@gmail.com>
2023-10-06 08:59:34 -0400

    Add MailClone component

--- src/vanilla/exampleTypeScript.js
+++ src/vanilla/exampleTypeScript.js
@@ -1,6 +1,9 @@
 var sport = 'football';
 var id = 5;
 id = 10;
+var hasDog = true;
+var hasCat;
 var firstName = 'Danny';
 console.log(firstName.toLowerCase());
 console.log(firstName);
+hasCat = false;
--- src/vanilla/exampleTypeScript.ts
+++ src/vanilla/exampleTypeScript.ts
@@ -1,7 +1,10 @@
 let sport: string = 'football';
 let id: number = 5;
 id = 10;
+let hasDog = true;
+let hasCat;
 
 let firstName = 'Danny';
 console.log(firstName.toLowerCase());
 console.log(firstName);
+hasCat = false;
