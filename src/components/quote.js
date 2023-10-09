ab8fd811a4e05048d9793bd06462a8e34f5108de
surajjayraman <suraj.jayraman@gmail.com>
2023-10-09 13:07:55 -0400

    Update Quote component dependency array

--- src/components/quote.js
+++ src/components/quote.js
@@ -3,21 +3,29 @@ import axios from 'axios';
 
 const Quote = () => {
     const [data, setData]= useState({});
+    const [count, setCount] = useState(0);
 
     useEffect(() => {
         axios.get('https://api.kanye.rest/')
             .then(res => {
-                console.log(res.data);
                 setData(res.data);
             })
-    },[])
+    },[count])
 
     return (
         <>
         <p className='highlighted'>Famous and funny quotes!</p>
-        <ul>
-            <li>{data.quote}</li>
-        </ul>
+        <div>
+            {count} {' '}
+            <button onClick={() => {
+                setCount(count => count + 1);
+             }
+            }>
+                +
+            </button>
+            <br/>
+            {data.quote}
+        </div>
         </>
     )
 
