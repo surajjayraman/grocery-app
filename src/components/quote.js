7d324e313a221aa0fae0c439c892a5b5b1fe04d6
surajjayraman <suraj.jayraman@gmail.com>
2023-10-09 12:46:59 -0400

    Update Quote component

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,18 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
 
 const Quote = () => {
+    const [data, setData]= useState({});
 
     useEffect(() => {
         axios.get('https://api.kanye.rest/')
             .then(res => {
                 console.log(res.data);
+                setData(res.data);
             })
     },[])
 
     return (
         <>
         <p className='highlighted'>Famous and funny quotes!</p>
+        <ul>
+            <li>{data.quote}</li>
+        </ul>
         </>
     )
 
