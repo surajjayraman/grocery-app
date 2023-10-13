41ce1403d41b6f4f4866eb61b7fce79aeb7d004c
surajjayraman <suraj.jayraman@gmail.com>
2023-10-13 11:23:46 -0400

    Update useAxios custom hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -5,19 +5,21 @@ import { useEffect, useState } from "react"
 const useAxios = (url) => {
     const [body, setBody] = useState('');
     const [error, setError] = useState(null);
+    const [pending, setPending] = useState(true);
 
     useEffect(() => {
+        setPending(true);
         axios.get(url)
             .then(res => {
                 setBody(res.data);
             })
             .catch(err => {
                 setError(err.message);
-
-            });
+            })
+            .finally(() => setPending(false));
     }, [url])
 
-    return [body, error];
+    return [body, error, pending];
 }
 
 export default useAxios;
