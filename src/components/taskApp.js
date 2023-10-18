1ab2a7cac3260de22c13bfa4269b6fedc1130845
surajjayraman <suraj.jayraman@gmail.com>
2023-10-18 19:39:31 -0400

    Refactor TaskApp

--- src/components/taskApp.js
+++ src/components/taskApp.js
@@ -9,36 +9,6 @@ const initialTasks = [
   {id: 2, text: 'Lennon Wall pic', done: false},
 ];
 
-function tasksReducer(tasks, action) {
-    switch (action.type) {
-      case 'added': {
-        return [
-          ...tasks,
-          {
-            id: action.id,
-            text: action.text,
-            done: false,
-          },
-        ];
-      }
-      case 'changed': {
-        return tasks.map((t) => {
-          if (t.id === action.task.id) {
-            return action.task;
-          } else {
-            return t;
-          }
-        });
-      }
-      case 'deleted': {
-        return tasks.filter((t) => t.id !== action.id);
-      }
-      default: {
-        throw Error('Unknown action: ' + action.type);
-      }
-    }
-  }
-
 export default function TaskApp() {
   const [tasks, setTasks] = useState(initialTasks);
 
