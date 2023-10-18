2161b6e3908dcb7a32947a1eb55c260e6eb2a00c
surajjayraman <suraj.jayraman@gmail.com>
2023-10-18 19:06:40 -0400

    Add reducer function to TaskApp

--- src/components/taskApp.js
+++ src/components/taskApp.js
@@ -9,6 +9,31 @@ const initialTasks = [
   {id: 2, text: 'Lennon Wall pic', done: false},
 ];
 
+function tasksReducer(tasks, action) {
+    if (action.type === 'added') {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    } else if (action.type === 'changed') {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    } else if (action.type === 'deleted') {
+      return tasks.filter((t) => t.id !== action.id);
+    } else {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+
 export default function TaskApp() {
   const [tasks, setTasks] = useState(initialTasks);
 
