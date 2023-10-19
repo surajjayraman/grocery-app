89bff891ee1488d9fd0dc4177ae82948fd186b7b
surajjayraman <suraj.jayraman@gmail.com>
2023-10-19 14:24:09 -0400

    Add ContextPage component

---
+++ src/components/contextPage.js
@@ -0,0 +1,15 @@
+import Heading from './heading.js';
+import Section from './section.js';
+
+export default function ContextPage() {
+  return (
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Heading level={2}>Heading</Heading>
+      <Heading level={3}>Sub-heading</Heading>
+      <Heading level={4}>Sub-sub-heading</Heading>
+      <Heading level={5}>Sub-sub-sub-heading</Heading>
+      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+    </Section>
+  );
+}
