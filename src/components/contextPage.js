fd3ff880308845499f04eaaa33c429c68b39c65a
surajjayraman <suraj.jayraman@gmail.com>
2023-10-19 15:29:30 -0400

    Update ContextPage component

--- src/components/contextPage.js
+++ src/components/contextPage.js
@@ -7,11 +7,21 @@ export default function ContextPage() {
     <p className='highlighted'>Context: an alternative to passing props</p>
     <Section>
       <Heading level={1}>Title</Heading>
-      <Heading level={2}>Heading</Heading>
-      <Heading level={3}>Sub-heading</Heading>
-      <Heading level={4}>Sub-sub-heading</Heading>
-      <Heading level={5}>Sub-sub-sub-heading</Heading>
-      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+      <Section>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Section>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Section>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
     </Section>
     </>
   );
