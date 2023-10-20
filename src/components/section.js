6e5098255d8e5d66fcc66038925afd4b081f3f2d
surajjayraman <suraj.jayraman@gmail.com>
2023-10-19 20:05:57 -0400

    Update Section component with context provider

--- src/components/section.js
+++ src/components/section.js
@@ -1,7 +1,12 @@
-export default function Section({ children }) {
+import { LevelContext } from "./levelContext";
+
+export default function Section({ level, children }) {
+
     return (
       <section className="section">
-        {children}
+        <LevelContext.Provider value={level}>
+            {children}
+        </LevelContext.Provider>
       </section>
     );
   }
