666fd78a6718b92462b20f0a467594e5170edf66
surajjayraman <suraj.jayraman@gmail.com>
2023-10-24 21:35:35 -0400

    Refactor Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,47 @@
 import { useState } from "react";
 
+const testData = [
+    {uid: 'aaa', name: 'Ali Barber'},
+    {uid: 'bbb', name: 'Jim Holden'}
+]
 
 const Header = (props) => {
     const [input, setInput] = useState('');
+    const [data, setData] = useState([]);
+
+      const addFriends = () => {
+
+        setData([
+            ...testData,
+            {
+                uid: 'ccc',
+                name: input
+            }
+        ]);
+
+    }
 
     return (
         <>
-        <p className="highlighted">React Review - My Friends App</p>
-        <h2>{props.text}</h2>
-        <div className="input-form">
-            <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
-        </div>
-        {input}
+            <p className="highlighted">React Review - My Friends App</p>
+            <h2>{props.text}</h2>
+            <div className="input-form">
+                <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+            </div>
+            {input} <br/>
+            <button type='submit' onClick={addFriends}>
+                Add friend
+            </button>
+            <ul>
+                    {data.map(item => {
+                        return (
+                            <li>
+                                {item.name}
+                            </li>
+                            )
+                        })
+                    }
+            </ul>
         </>
     )
 
