355116c7bafc26f759e30cf5d04725d66aa1b3f4
surajjayraman <suraj.jayraman@gmail.com>
2023-10-27 12:51:57 -0400

    Update Toggle component

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 
 export default function Toggle() {
-  const [isOnRef, setIsOnRef] = useState(false);
+  const [isOn, setIsOn] = useState(false);
 
   return (
     <>
     <p className='highlighted'>Fix a component failing to re-render </p>
     <button onClick={() => {
-      setIsOnRef(!isOnRef)
+      setIsOn(!isOn)
     }}>
-      {isOnRef ? 'On' : 'Off'}
+      {isOn ? 'On' : 'Off'}
     </button>
     </>
   );
