bddbdf1e276248f8274008039b73e632727ebe25
surajjayraman <suraj.jayraman@gmail.com>
2023-10-31 17:41:41 -0400

    Add list.test jest tests

---
+++ src/__test__/list.test.js
@@ -0,0 +1,26 @@
+const { getFriendCount } = require("../helpers/list");
+
+const data = [
+    { name: "Betty White", uid: "1" },
+    { name: "Freddy Mercury", uid: "2" },
+    { name: "James Holden", uid: "3" },
+    { name: "Tom Cruise", uid: "4" },
+];
+
+describe('Helper tests', () => {
+
+    //---------------------------------------
+    test("returns empty array if no friends", () => {
+        const count = getFriendCount(null);
+        expect(count).toBe(-1);
+    });
+
+    test("returns count of friends", () => {
+        const count = getFriendCount(data);
+
+        expect(count).toBe(4);
+    });
+
+
+
+});
