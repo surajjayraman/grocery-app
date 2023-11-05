923adeebfb6627298490527f92276ab277253139
surajjayraman <suraj.jayraman@gmail.com>
2023-11-05 09:37:37 -0500

    Add FriendList prettyDom list jest tests

--- src/components/__test__/friendList.test.js
+++ src/components/__test__/friendList.test.js
@@ -24,10 +24,9 @@ describe('FriendList Tests', () => {
   it("renders with data and renders a list", () => {
     const {container, getByRole} = render(<FriendList items={data} />);
 
-    console.log(prettyDOM(container));
-
     const list = screen.getByRole("list");
     // const list1 = getByRole("list");  // Newer version does not like this!
+    console.log(prettyDOM(list));
 
     expect(list).toBeTruthy();
   });
