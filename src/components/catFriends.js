89852a15b07a4d1f70c1dfeee191a530048920f9
surajjayraman <suraj.jayraman@gmail.com>
2023-11-17 13:42:19 -0500

    Update CatFriends component

--- src/components/catFriends.js
+++ src/components/catFriends.js
@@ -1,73 +1,71 @@
 import { useRef } from 'react';
 
 export default function CatFriends() {
-    const firstCatRef = useRef(null);
-    const secondCatRef = useRef(null);
-    const thirdCatRef = useRef(null);
+    const itemsRef = useRef(null);
 
-    function handleScrollToFirstCat() {
-        firstCatRef.current.scrollIntoView({
+    function scrollToId(itemId) {
+        const map = getMap();
+        const node = map.get(itemId);
+        node.scrollIntoView({
             behavior: 'smooth',
             block: 'nearest',
             inline: 'center'
         });
     }
 
-    function handleScrollToSecondCat() {
-        secondCatRef.current.scrollIntoView({
-            behavior: 'smooth',
-            block: 'nearest',
-            inline: 'center'
-        });
-    }
-
-    function handleScrollToThirdCat() {
-        thirdCatRef.current.scrollIntoView({
-            behavior: 'smooth',
-            block: 'nearest',
-            inline: 'center'
-        });
+    function getMap() {
+        if (!itemsRef.current) {
+            // Initialize the Map on first usage.
+            itemsRef.current = new Map();
+        }
+        return itemsRef.current;
     }
 
     return (
         <>
-            <p className='highlighted'>Scrolling to an element</p>
+            <p className='highlighted'>How to manage a list of refs using a ref callback</p>
             <nav>
-                <button onClick={handleScrollToFirstCat}>
+                <button onClick={() => scrollToId(0)}>
                     Tom
                 </button>
-                <button onClick={handleScrollToSecondCat}>
+                <button onClick={() => scrollToId(5)}>
                     Maru
                 </button>
-                <button onClick={handleScrollToThirdCat}>
+                <button onClick={() => scrollToId(9)}>
                     Jellylorum
                 </button>
             </nav>
             <div>
                 <ul>
-                    <li>
-                        <img
-                            src="https://placekitten.com/g/200/200"
-                            alt="Tom"
-                            ref={firstCatRef}
-                        />
-                    </li>
-                    <li>
-                        <img
-                            src="https://placekitten.com/g/300/200"
-                            alt="Maru"
-                            ref={secondCatRef}
-                        />
-                    </li>
-                    <li>
-                        <img
-                            src="https://placekitten.com/g/250/200"
-                            alt="Jellylorum"
-                            ref={thirdCatRef}
-                        />
-                    </li>
+                    {catList.map(cat => (
+                        <li
+                            key={cat.id}
+                            ref={(node) => {
+                                const map = getMap();
+                                if (node) {
+                                    map.set(cat.id, node);
+                                } else {
+                                    map.delete(cat.id);
+                                }
+                            }}
+                        >
+                            <img
+                                src={cat.imageUrl}
+                                alt={'Cat #' + cat.id}
+                            />
+                        </li>
+                    ))}
                 </ul>
             </div>
         </>
     );
 }
+
+const catList = [];
+for (let i = 0; i < 10; i++) {
+    catList.push({
+        id: i,
+        imageUrl: 'https://placekitten.com/250/200?image=' + i
+    });
+}
+
