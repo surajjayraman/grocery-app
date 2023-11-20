56593139b5dbafa995c8b79c50dfbc48828a0c7d
surajjayraman <suraj.jayraman@gmail.com>
2023-11-20 08:52:08 -0500

    Update VideoPlayer component

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -2,10 +2,17 @@ import { useState, useRef } from 'react';
 
 export default function VideoPlayer() {
     const [isPlaying, setIsPlaying] = useState(false);
+    const ref = useRef(null);
 
     function handleClick() {
         const nextIsPlaying = !isPlaying;
         setIsPlaying(nextIsPlaying);
+
+        if (nextIsPlaying) {
+            ref.current.play();
+        } else {
+            ref.current.pause();
+        }
     }
 
     return (
@@ -14,7 +21,12 @@ export default function VideoPlayer() {
             <button onClick={handleClick}>
                 {isPlaying ? 'Pause' : 'Play'}
             </button>
-            <video width="250">
+            <video
+                width="250"
+                ref={ref}
+                onPlay={() => setIsPlaying(true)}
+                onPause={() => setIsPlaying(false)}
+            >
                 <source
                     src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
                     type="video/mp4"
@@ -23,3 +35,5 @@ export default function VideoPlayer() {
         </>
     )
 }
+
+
