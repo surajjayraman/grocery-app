ad79dee815acbcf52a900f7c3731eb3aaac537b5
surajjayraman <suraj.jayraman@gmail.com>
2023-11-24 16:05:26 -0500

    Add fetchBio API

---
+++ src/components/api.js
@@ -0,0 +1,10 @@
+export async function fetchBio(person) {
+    const delay = person === 'Bob' ? 2000 : 200;
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('This is ' + person + '’s bio.');
+      }, delay);
+    })
+  }
+  
+  
