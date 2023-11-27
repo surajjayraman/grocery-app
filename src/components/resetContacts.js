bd74ac386de796b11255763b33ea65f0a3d18e2e
surajjayraman <suraj.jayraman@gmail.com>
2023-11-27 14:54:16 -0500

    Update ResetContact component

--- src/components/resetContacts.js
+++ src/components/resetContacts.js
@@ -1,51 +1,52 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-export default function ResetContact({ savedContact, onSave }) {
-    const [name, setName] = useState(savedContact.name);
-    const [email, setEmail] = useState(savedContact.email);
+export default function ResetContact(props) {
+  return (
+    <EditForm
+      {...props}
+      key={props.savedContact.id}
+    />
+  );
+}
 
-    useEffect(() => {
+function EditForm({ savedContact, onSave }) {
+  const [name, setName] = useState(savedContact.name);
+  const [email, setEmail] = useState(savedContact.email);
+
+  return (
+    <section>
+      <label>
+        Name:{' '}
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Email:{' '}
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </label>
+      <button onClick={() => {
+        const updatedData = {
+          id: savedContact.id,
+          name: name,
+          email: email
+        };
+        onSave(updatedData);
+      }}>
+        Save
+      </button>
+      <button onClick={() => {
         setName(savedContact.name);
         setEmail(savedContact.email);
-    }, [savedContact]);
-
-    return (
-        <>
-            <p className='highlighted'>Reset state without Effects</p>
-            <section>
-                <label>
-                    Name:{' '}
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Email:{' '}
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </label>
-                <button onClick={() => {
-                    const updatedData = {
-                        id: savedContact.id,
-                        name: name,
-                        email: email
-                    };
-                    onSave(updatedData);
-                }}>
-                    Save
-                </button>
-                <button onClick={() => {
-                    setName(savedContact.name);
-                    setEmail(savedContact.email);
-                }}>
-                    Reset
-                </button>
-            </section>
-        </>
-    );
+      }}>
+        Reset
+      </button>
+    </section>
+  );
 }
