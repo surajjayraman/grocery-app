be39624a315ab5ea5bf22c8b5e6192b05f344482
surajjayraman <suraj.jayraman@gmail.com>
2023-12-10 09:29:48 -0500

    Update variables in types.ts

--- src/learn-typescript/types.js
+++ src/learn-typescript/types.js
@@ -1,3 +1,8 @@
 var n = 1;
 //n = 'Bobb';
 n = 12356;
+var isDone = false;
+function statusCheck(isDone) {
+    isDone ? console.log('All set good to go !') : console.log('Still working on..!');
+}
+statusCheck(isDone);
--- src/learn-typescript/types.ts
+++ src/learn-typescript/types.ts
@@ -1,4 +1,11 @@
-var n : number = 1;
+var n: number = 1;
 
 //n = 'Bobb';
 n = 12356;
+let isDone: boolean = false;
+
+function statusCheck(isDone) {
+    isDone ? console.log('All set good to go !') : console.log('Still working on..!')
+}
+
+statusCheck(isDone);
