4c56d4741f829fa97bca922963bf292d1e6cd637
surajjayraman <suraj.jayraman@gmail.com>
2024-01-04 14:44:51 -0500

    Update mixin functionality assign

--- src/learn-patterns/factory/mixinPattern.js
+++ src/learn-patterns/factory/mixinPattern.js
@@ -9,3 +9,12 @@ const dogFunctionality = {
     wagTail: () => console.log("Wagging my tail!"),
     play: () => console.log("Playing!"),
 };
+
+Object.assign(Dog.prototype, dogFunctionality)
+
+const pet1 = new Dog("Daisy");
+
+pet1.name; // Daisy
+pet1.bark(); // Woof!
+pet1.play(); // Playing!
+
