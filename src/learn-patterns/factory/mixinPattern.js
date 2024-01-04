4566bdb01755c874fcec0f3c81f0fad9f246af5a
surajjayraman <suraj.jayraman@gmail.com>
2024-01-04 15:18:49 -0500

    Update mixin animal functionality

--- src/learn-patterns/factory/mixinPattern.js
+++ src/learn-patterns/factory/mixinPattern.js
@@ -4,6 +4,11 @@ class Dog {
     }
 }
 
+const animalFunctionality = {
+    walk: () => console.log("Waking!"),
+    sleep: () => console.log("Sleeping!")
+}
+
 const dogFunctionality = {
     bark: () => console.log("Woof!"),
     wagTail: () => console.log("Wagging my tail!"),
@@ -17,4 +22,7 @@ const pet1 = new Dog("Daisy");
 pet1.name; // Daisy
 pet1.bark(); // Woof!
 pet1.play(); // Playing!
+
+animalFunctionality.walk()
+animalFunctionality.sleep()
 
