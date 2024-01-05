f7e1bf10ee8c6d6533b37f8cec47532a37c8e871
surajjayraman <suraj.jayraman@gmail.com>
2024-01-05 12:25:53 -0500

    Refactor module pattern

--- src/learn-patterns/factory/modulePattern.js
+++ src/learn-patterns/factory/modulePattern.js
@@ -1,6 +1,5 @@
 // import properties from mathData module
 const { add, multiply, subtract, square } = require("./mathData")
-
 console.log(add(2, 3));
 console.log(multiply(2));
 console.log(subtract(2, 3));
