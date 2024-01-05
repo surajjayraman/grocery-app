98ea3469099932dad91b155830d56241e21fb680
surajjayraman <suraj.jayraman@gmail.com>
2024-01-05 09:22:58 -0500

    Update math data

---
+++ src/learn-patterns/factory/mathData.js
@@ -0,0 +1,12 @@
+function add(x, y) {
+    return x + y;
+}
+function multiply(x) {
+    return x * 2;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function square(x) {
+    return x * x;
+}
