f3cf80917bd3b3d55ec9abd0beb6743c76fd4ced
surajjayraman <suraj.jayraman@gmail.com>
2024-01-07 18:10:10 -0500

    Update prototypePattern

--- src/learn-patterns/factory/prototypePattern.js
+++ src/learn-patterns/factory/prototypePattern.js
@@ -8,10 +8,21 @@ class Dog {
     }
 }
 
+class SuperDog extends Dog {
+    constructor(name) {
+        super(name);
+    }
+
+    fly() {
+        console.log("Flying!");
+    }
+}
+
 const dog1 = new Dog("Daisy");
-const dog2 = new Dog("Max");
+const dog2 = new SuperDog("Max");
 const dog3 = new Dog("Spot");
 
 Dog.prototype.play = () => console.log("Playing now!");
 
 dog1.play();
+dog2.fly();
