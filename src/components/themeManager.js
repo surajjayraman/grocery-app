bbb2e11964742492c227256e3cdb16025e51a510
surajjayraman <suraj.jayraman@gmail.com>
2024-01-08 14:09:51 -0500

    Update themeManager

--- src/components/themeManager.js
+++ src/components/themeManager.js
@@ -1,4 +1,13 @@
 import { useState } from "react";
 import "./themes.css"
 
-
+export const themes = {
+    light: {
+        background: "#fff",
+        color: "#000"
+    },
+    dark: {
+        background: "#171717",
+        color: "#fff"
+    }
+};
