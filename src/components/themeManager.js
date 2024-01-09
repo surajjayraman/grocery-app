374a25bff07ce735384abf7d49824304a6fb245d
surajjayraman <suraj.jayraman@gmail.com>
2024-01-09 13:12:57 -0500

    Refactor themeManager with theme provider

--- src/components/themeManager.js
+++ src/components/themeManager.js
@@ -16,7 +16,7 @@ export const themes = {
 
 export const ThemeContext = React.createContext()
 
-// refactor
+// refactor with hooks
 function useThemeContext() {
     const theme = useContext(ThemeContext);
     if (!theme) {
@@ -24,6 +24,25 @@ function useThemeContext() {
     }
     return theme;
 }
+function ThemeProvider({ children }) {
+    const [theme, setTheme] = useState("dark");
+
+    function toggleTheme() {
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+
+    const providerValue = {
+        theme: themes[theme],
+        toggleTheme,
+    };
+
+    return (
+        <ThemeContext.Provider value={providerValue}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
 
 export default function ThemeManager() {
     const [theme, setTheme] = useState('dark')
