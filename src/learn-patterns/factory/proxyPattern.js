6147bc2eff3bcad7e7cdb415ca318dafd37f8887
surajjayraman <suraj.jayraman@gmail.com>
2024-01-10 11:22:31 -0500

    Add person proxy handlers

--- src/learn-patterns/factory/proxyPattern.js
+++ src/learn-patterns/factory/proxyPattern.js
@@ -9,4 +9,13 @@ const person = {
 
 // create a proxy object
 
-const personProxy = new Proxy(person, {})
+const personProxy = new Proxy(person, {
+    get: (obj, prop) => {
+        console.log(`The value of ${prop} is ${obj[prop]}`);
+    },
+    set: (obj, prop, value) => {
+        console.log(`Changed ${prop} from ${obj[prop]} to ${value}`);
+        obj[prop] = value;
+        return true;
+    }
+})
