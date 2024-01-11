07a9869b33aa80aff3be9594153d6d88f2fc526c
surajjayraman <suraj.jayraman@gmail.com>
2024-01-11 11:38:43 -0500

    Update class counter

--- src/learn-patterns/factory/singletonPattern.js
+++ src/learn-patterns/factory/singletonPattern.js
@@ -1,24 +1,31 @@
 let counter = 0;
+let instance;
 
 class Counter {
-  getInstance() {
-    return this;
-  }
+    constructor() {
+        if (instance) {
+            throw new Error("You can only create one instance!!");
+        }
+        instance = this
+    }
+    getInstance() {
+        return this;
+    }
 
-  getCount() {
-    return counter;
-  }
+    getCount() {
+        return counter;
+    }
 
-  increment() {
-    return ++counter;
-  }
+    increment() {
+        return ++counter;
+    }
 
-  decrement() {
-    return --counter;
-  }
+    decrement() {
+        return --counter;
+    }
 }
 
 const counter1 = new Counter()
 const counter2 = new Counter()
 
-console.log(counter1 === counter2)
+console.log(counter1.getInstance() === counter2.getInstance())
