891159ac2a872b70e623a176da2714ca0dab47f0
surajjayraman <suraj.jayraman@gmail.com>
2024-01-22 08:21:32 -0500

    Add testLuck promise

--- src/learn-patterns/factory/groupBy.js
+++ src/learn-patterns/factory/groupBy.js
@@ -1,85 +1,101 @@
 require("core-js/actual/array/group-by");
 
 const people = [
-    { name: "kyle", age: 28 },
-    { name: "Sally", age: 45 },
-    { name: "John", age: 49 },
-    { name: "Sally", age: 28 }
-]
+  { name: "kyle", age: 28 },
+  { name: "Sally", age: 45 },
+  { name: "John", age: 49 },
+  { name: "Sally", age: 28 },
+];
 
 // group users
-const groupedNames = people.groupBy(person => {
-    return person.name
-})
+const groupedNames = people.groupBy((person) => {
+  return person.name;
+});
 
 console.log(groupedNames);
 
 // sort users
 const users = [
-    { name: "kyle", age: 28 },
-    { name: "Sally", age: 45 },
-    { name: "John", age: 49 },
-    { name: "Sally", age: 28 }
-]
+  { name: "kyle", age: 28 },
+  { name: "Sally", age: 45 },
+  { name: "John", age: 49 },
+  { name: "Sally", age: 28 },
+];
 function sortByName(users) {
-    return users.sort((userA, userB) => {
-        if (userA.name.toLowerCase() < userB.name.toLowerCase()) return -1
-        if (userA.name.toLowerCase() > userB.name.toLowerCase()) return 1
-        return 0
-    })
+  return users.sort((userA, userB) => {
+    if (userA.name.toLowerCase() < userB.name.toLowerCase()) return -1;
+    if (userA.name.toLowerCase() > userB.name.toLowerCase()) return 1;
+    return 0;
+  });
 }
 
-const sortedUsers = sortByName(users)
-console.log(sortedUsers)
+const sortedUsers = sortByName(users);
+console.log(sortedUsers);
 
 // regular array sort
-const months = ['March', 'Jan', 'Feb', 'Dec'];
+const months = ["March", "Jan", "Feb", "Dec"];
 months.sort();
 console.log(months);
 
 // use of Map
 const userMap = new Map([
-    [1, { name: 'kyle' }],
-    [2, { name: 'Sally' }]
-])
+  [1, { name: "kyle" }],
+  [2, { name: "Sally" }],
+]);
 
-console.log(userMap)
-userMap.set(3, { name: 'John' })
-console.log(userMap)
+console.log(userMap);
+userMap.set(3, { name: "John" });
+console.log(userMap);
 
 // functions are just variables
 
-function test() {
-
-}
-const test2 = test
-console.log(test2 === test)
-console.log(test)
-console.dir(test)
+function test() {}
+const test2 = test;
+console.log(test2 === test);
+console.log(test);
+console.dir(test);
 
 // Big O
 
 function sum_char_codes(n) {
-    let sum = 0;
-    for (let i = 0; i < n.length; ++i) {
-        sum += n.charCodeAt(i);
-    }
+  let sum = 0;
+  for (let i = 0; i < n.length; ++i) {
+    sum += n.charCodeAt(i);
+  }
 
-    return sum;
+  return sum;
 }
 
-console.log(sum_char_codes('suraj'))
+console.log(sum_char_codes("suraj"));
 
-const a = new ArrayBuffer(6)
-console.log(a)
+const a = new ArrayBuffer(6);
+console.log(a);
 
 // promise demo
 const promise1 = new Promise((resolve, reject) => {
-    resolve('Success!');
+  resolve("Success!");
 });
 
 promise1.then((value) => {
-    console.log(value);
-    // Expected output: "Success!"
+  console.log(value);
+  // Expected output: "Success!"
 });
 
+// Creating a new Promise and saving it to the testLuck variable.
+// Two arguments are being passed, one for when the promise resolves,
+// and one for if the promise gets rejected.
+
+const testLuck = new Promise((resolve, reject) => {
+  if (Math.random() < 0.5) {
+    resolve("Lucky Winner");
+  }
+  reject(new Error("Unlucky!"));
+});
+
+testLuck
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
