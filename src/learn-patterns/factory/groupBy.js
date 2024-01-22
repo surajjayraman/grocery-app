a9fbac9baec6c795724368e128c990c7dba79679
surajjayraman <suraj.jayraman@gmail.com>
2024-01-22 10:44:53 -0500

    Add async await promise

--- src/learn-patterns/factory/groupBy.js
+++ src/learn-patterns/factory/groupBy.js
@@ -122,3 +122,19 @@ orderSunglasses
   .catch((error) => {
     console.log(error);
   });
+
+// Creating a new promise that runs the function in the setTimeout after 5 seconds.
+const newPromise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("All done!"), 5000);
+});
+
+// Creating an asynchronous function using an arrow expression
+// and saving it to a the variable asyncFunction.
+const asyncFunction = async () => {
+  // Awaiting the promise to resolve and
+  // saving the result to the variable finalResult.
+  const finalResult = await newPromise;
+  console.log(finalResult);
+};
+
+asyncFunction();
