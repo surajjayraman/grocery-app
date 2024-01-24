72c8c0dd7b9ccbf838b159d3a4812b03f678ec38
surajjayraman <suraj.jayraman@gmail.com>
2024-01-23 20:58:11 -0500

    Add setTimeout

--- src/learn-patterns/factory/groupBy.js
+++ src/learn-patterns/factory/groupBy.js
@@ -138,3 +138,16 @@ const asyncFunction = async () => {
 };
 
 asyncFunction();
+
+console.log("This is the first line of code.");
+// Keep the line above as the first line of code
+// Write your code here:
+
+function usingSTO() {
+  console.log("test string");
+}
+
+setTimeout(usingSTO, 2500);
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code.");
