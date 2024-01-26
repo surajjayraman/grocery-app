20e0b59eac256736367eb59a6fb09ed20c332a95
surajjayraman <suraj.jayraman@gmail.com>
2024-01-26 09:41:12 -0500

    Update Library with checkAvailability

--- src/learn-patterns/factory/library.js
+++ src/learn-patterns/factory/library.js
@@ -19,4 +19,20 @@ const checkInventory = (order) => {
   });
 };
 
+const checkAvailability = (itemName, distributorName) => {
+  console.log(`Checking availability of ${itemName} at ${distributorName}...`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (restockSuccess()) {
+        console.log(`${itemName} are in stock at ${distributorName}`);
+        resolve(itemName);
+      } else {
+        reject(
+          `Error: ${itemName} is unavailable from ${distributorName} at this time.`
+        );
+      }
+    }, 1000);
+  });
+};
+
 module.exports = { checkInventory };
