bcac332ffbfe9ad8f04b03fe80830888835d869e
surajjayraman <suraj.jayraman@gmail.com>
2024-01-26 10:24:42 -0500

    Update promiseAll js with failure onReject

--- src/learn-patterns/factory/promiseAll.js
+++ src/learn-patterns/factory/promiseAll.js
@@ -7,6 +7,10 @@ const onFulfill = (itemsArray) => {
   );
 };
 
+const onReject = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+
 const checkSunglasses = checkAvailability("sunglasses", "Favorite Supply Co.");
 const checkPants = checkAvailability("pants", "Favorite Supply Co.");
 const checkBags = checkAvailability("bags", "Favorite Supply Co.");
