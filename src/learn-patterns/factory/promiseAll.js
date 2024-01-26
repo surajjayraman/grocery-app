4d35f9ad63c8285c96451ec943723e4fccdceb5d
surajjayraman <suraj.jayraman@gmail.com>
2024-01-26 10:22:49 -0500

    Update promiseAll js with checkBags

--- src/learn-patterns/factory/promiseAll.js
+++ src/learn-patterns/factory/promiseAll.js
@@ -2,3 +2,4 @@ const { checkAvailability } = require("./library");
 
 const checkSunglasses = checkAvailability("sunglasses", "Favorite Supply Co.");
 const checkPants = checkAvailability("pants", "Favorite Supply Co.");
+const checkBags = checkAvailability("bags", "Favorite Supply Co.");
