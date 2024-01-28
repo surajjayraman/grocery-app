fe216713a0e3e2d3111eaae35c7e2c968f3f5f3d
surajjayraman <suraj.jayraman@gmail.com>
2024-01-28 15:24:41 -0500

    Update promisifiedReadApp with callback fns

--- src/learn-patterns/factory/promisifiedReadApp.js
+++ src/learn-patterns/factory/promisifiedReadApp.js
@@ -9,7 +9,7 @@ fs.readFile('./file1.txt', 'utf-8', (err, data) => {
 
 // Here we use native promises with our "promisified" version of readfile:
 let firstSentence;
-promisifiedReadfile("./file1.txt", "utf-8")
+promisifiedReadfile("./file.txt", "utf-8")
   .then((data) => {
     firstSentence = data;
     console.log(firstSentence);
