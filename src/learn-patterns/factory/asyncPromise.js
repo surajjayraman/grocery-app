749a63086d53a1c429932c8814599e3c193922f7
surajjayraman <suraj.jayraman@gmail.com>
2024-01-29 19:15:53 -0500

    Add withConstructor function

--- src/learn-patterns/factory/asyncPromise.js
+++ src/learn-patterns/factory/asyncPromise.js
@@ -2,6 +2,14 @@ const numberPromise = async () => {
   return Math.random();
 };
 
-numberPromise()
-.then((data) => console.log(data))
+numberPromise().then((data) => console.log(data));
 
+function withConstructor(num) {
+  return new Promise((resolve, reject) => {
+    if (num === 0) {
+      resolve("zero");
+    } else {
+      resolve("not zero");
+    }
+  });
+}
