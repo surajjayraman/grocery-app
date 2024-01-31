ef1492178687ca907e04fce369f2e6402f377bba
surajjayraman <suraj.jayraman@gmail.com>
2024-01-30 19:47:13 -0500

    Update dinnerPromise with async await version

--- src/learn-patterns/factory/dinnerPromise.js
+++ src/learn-patterns/factory/dinnerPromise.js
@@ -4,3 +4,12 @@ const brainstormDinner = require("./dinnerLibrary");
 brainstormDinner().then((meal) =>
   console.log(`I will be cooking ${meal} for dinner`)
 );
+
+// async/await version:
+async function announceDinner() {
+  const meal = await brainstormDinner();
+  console.log(`I will be cooking ${meal} for dinner`);
+}
+
+announceDinner()
+
