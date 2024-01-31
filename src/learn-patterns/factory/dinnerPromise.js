6de4b5f8370701a39dc6117f391e5131589e788b
surajjayraman <suraj.jayraman@gmail.com>
2024-01-30 19:32:42 -0500

    Update dinnerPromise with native promise version

--- src/learn-patterns/factory/dinnerPromise.js
+++ src/learn-patterns/factory/dinnerPromise.js
@@ -1,1 +1,6 @@
-const brainstormDinner = require('./dinnerLibrary')
+const brainstormDinner = require("./dinnerLibrary");
+
+//Native promise version
+brainstormDinner().then((meal) =>
+  console.log(`I will be cooking ${meal} for dinner`)
+);
