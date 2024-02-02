26a7fa575b3ef2b51732de6fe5d9da6bad93a160
surajjayraman <suraj.jayraman@gmail.com>
2024-02-02 15:19:13 -0500

    Update nativeMethodsExp with linear TC func

--- src/learn-patterns/factory/nativeMethodsExp.js
+++ src/learn-patterns/factory/nativeMethodsExp.js
@@ -7,3 +7,15 @@ console.log(newArr, arr, arr[0]);
 
 const obj = { a: 1 };
 console.log(obj.a);
+
+// what is the TC
+const countChars = (str) => {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    count++;
+  }
+  return count;
+};
+
+console.log(countChars("Today is a friday!")); // TC = O(n)
