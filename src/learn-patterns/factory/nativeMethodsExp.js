fac084fc3bf544b20202e5b9618205757bc1d269
surajjayraman <suraj.jayraman@gmail.com>
2024-02-02 14:28:35 -0500

    Update nativeMethodsExp

--- src/learn-patterns/factory/nativeMethodsExp.js
+++ src/learn-patterns/factory/nativeMethodsExp.js
@@ -2,5 +2,8 @@
 // methods/expressions/operations?
 
 const arr = [1, 2, 3];
-const newArr = arr.pop(); // 3
+const newArr = arr.pop(); // 3 // constant time space complexity
 console.log(newArr, arr, arr[0]);
+
+const obj = { a: 1 };
+console.log(obj.a);
