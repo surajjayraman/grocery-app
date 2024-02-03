a7ee003fec3797512afea1ef7452ac8bbb4aaca9
surajjayraman <suraj.jayraman@gmail.com>
2024-02-03 08:15:47 -0500

    Add uniqSort func

--- src/learn-patterns/factory/nativeMethodsExp.js
+++ src/learn-patterns/factory/nativeMethodsExp.js
@@ -79,3 +79,15 @@ console.log(
     1, 2, 3, 4, 5, 6, 7, 8, 10000, 0.3, 9, 11, 12, 13, 14, 8, 255, 256, 257,
   ])
 );
+
+//Task: Transform this simple sorting algorithm into a unique sort.
+// It should not return any duplicate values in the sorted array.
+
+//input: [1,5,2,1] => output: [1,2,5]
+//input: [4,2,2,3,2,2,2] => output: [2,3,4]
+
+const uniqSort = (arr) => {
+  return arr.sort((a, b) => a - b);
+};
+
+console.log(uniqSort([4, 2, 2, 3, 2, 2, 2])); // => [2,3,4]
