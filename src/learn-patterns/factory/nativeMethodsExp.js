5c301e98895bc1a54c212b1a17c8483b8f3afb8e
surajjayraman <suraj.jayraman@gmail.com>
2024-02-03 10:10:44 -0500

    Add basicMemoization

--- src/learn-patterns/factory/nativeMethodsExp.js
+++ src/learn-patterns/factory/nativeMethodsExp.js
@@ -97,4 +97,4 @@ const uniqSort = (arr) => {
   return uniqSortArr;
 };
 
-console.log(uniqSort([4, 2, 2, 3, 2, 2, 2])); // => [2,3,4]
+console.log(uniqSort([4, 2, 2, 3, 2, 2, 2, 4, 3, 3, 3])); // => [2,3,4]
