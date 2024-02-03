d4ef57dfa9b275f550df72fa03354196d1572d43
surajjayraman <suraj.jayraman@gmail.com>
2024-02-03 11:47:04 -0500

    Update basicMemoization with memoTimes10

--- src/learn-patterns/factory/basicMemoization.js
+++ src/learn-patterns/factory/basicMemoization.js
@@ -4,5 +4,18 @@ const times10 = (n) => {
   return n * 10;
 };
 
-console.log('------Task1------');
-console.log(`times10 returns: ${times10(9)}`)
+console.log("------Task1------");
+console.log(`times10 returns: ${times10(9)}`);
+
+const cache = {};
+const memoTimes10 = (n) => {
+  if (!cache[n]) {
+    cache[n] = times10(n);
+  }
+  return cache[n];
+};
+
+console.log("------Task2------");
+console.log(`memotimes10 returns: ${memoTimes10(9)}`);
+console.log(`memotimes10 returns: ${memoTimes10(10)}`);
+console.log(`Cache holds: ${JSON.stringify(cache)}`)
