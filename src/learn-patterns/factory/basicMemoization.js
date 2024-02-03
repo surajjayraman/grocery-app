695ffc253a2b7c43e50243c46edc608a34b2f9fe
surajjayraman <suraj.jayraman@gmail.com>
2024-02-03 17:07:57 -0500

    Refactor memoTimes10 using closure

--- src/learn-patterns/factory/basicMemoization.js
+++ src/learn-patterns/factory/basicMemoization.js
@@ -22,3 +22,28 @@ console.log(`memotimes10 returns: ${memoTimes10(9)}`);
 console.log(`memotimes10 returns: ${memoTimes10(10)}`);
 console.log(`memotimes10 returns: ${memoTimes10(9)}`);
 console.log(`Cache holds: ${JSON.stringify(cache)}`);
+
+// Clean up the global scope
+// Use a closure to return a function that you can call later
+console.log("------Task3------");
+const memoizedClosureTimes10 = () => {
+  const cache = {};
+  const memoTimes10 = (n) => {
+    console.log(`Cache holds: ${JSON.stringify(cache)}, ${cache[n]}`);
+    if (!cache[n]) {
+      console.log("------Caching------");
+      cache[n] = times10(n);
+    }
+    return cache[n];
+  };
+  return memoTimes10;
+};
+
+const memoClosureTimes10 = memoizedClosureTimes10();
+
+try {
+  console.log(`Task3 calculated value: ${memoClosureTimes10(9)}`);
+  console.log(`Task3 cached value: ${memoClosureTimes10(9)}`);
+} catch (e) {
+  console.error(`Task3 error: ${e}`);
+}
