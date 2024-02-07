b8ee148cf602ede4bd8f84f4f5e2a0b9cac16dfb
surajjayraman <suraj.jayraman@gmail.com>
2024-02-07 15:13:57 -0500

    Add sorted array to binarySearch algo

--- src/learn-patterns/factory/searchAlgos.js
+++ src/learn-patterns/factory/searchAlgos.js
@@ -17,7 +17,17 @@ console.log(linearSearch([2, 55, 6, 77, 90, 6], 666));
 
 const binarySearch = (list, item) => {
   let index = -1;
+  let min = 0;
+  let max = list.length - 1;
+  console.log(`input array: ${list}`);
   return index;
 };
 
-console.log(binarySearch([2, 55, 6, 77, 90, 6], 6));
+console.log(
+  binarySearch(
+    [2, 55, 6, 77, 90, 6].sort((a, b) => {
+      return a - b;
+    }),
+    6
+  )
+);
