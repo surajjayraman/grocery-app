4800621bc3a2c546337fe560f6dcb121c29892da
surajjayraman <suraj.jayraman@gmail.com>
2024-02-07 15:05:11 -0500

    Add binarySearch algo

--- src/learn-patterns/factory/searchAlgos.js
+++ src/learn-patterns/factory/searchAlgos.js
@@ -12,3 +12,12 @@ function linearSearch(list, item) {
 
 console.log(linearSearch([2, 55, 6, 77, 90, 6], 6));
 console.log(linearSearch([2, 55, 6, 77, 90, 6], 666));
+
+// implement a binary search
+
+const binarySearch = (list, item) => {
+  let index = -1;
+  return index;
+};
+
+console.log(binarySearch([2, 55, 6, 77, 90, 6], 6));
