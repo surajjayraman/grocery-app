5624853b4ad0a4f5dffe4a4f9afca6b97731d9cc
surajjayraman <suraj.jayraman@gmail.com>
2024-02-07 14:35:39 -0500

    Refactor memoizeFunc

--- src/learn-patterns/factory/memoization.js
+++ src/learn-patterns/factory/memoization.js
@@ -3,4 +3,28 @@ const sayHello = (name) => {
   return `hello there ${name}`;
 };
 
-console.log(sayHello("suraj"));
+//console.log(sayHello("suraj"));
+
+const memoizeFunc = (cb) => {
+  const cache = {};
+  let result = "";
+
+  const wrapper = (...args) => {
+    console.log(`----Cache contents----: ${JSON.stringify(cache)}`);
+    if (!cache[args[0]]) {
+      result = cb(args[0]);
+      cache[args[0]] = result;
+      return result;
+    }
+    // retrieve from  cache
+    result = cache[args[0]];
+    return result;
+  };
+
+  return wrapper;
+};
+
+const sayHelloMemo = memoizeFunc(sayHello);
+console.log(sayHelloMemo("Sweety"));
+console.log(sayHelloMemo("Suraj"));
+console.log(sayHelloMemo("Suraj"));
