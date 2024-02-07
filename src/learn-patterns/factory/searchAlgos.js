d048f67b3bc3f2b6e8ca6314431e0430738d0769
surajjayraman <suraj.jayraman@gmail.com>
2024-02-06 20:30:49 -0500

    Update searchAlgos with linearSearch

--- src/learn-patterns/factory/searchAlgos.js
+++ src/learn-patterns/factory/searchAlgos.js
@@ -1,1 +1,13 @@
 // implement a linear search
+
+function linearSearch(list, item) {
+  let index = -1;
+  list.forEach((element, i) => {
+    if (element === item) {
+      index = i;
+    }
+  });
+  return index;
+}
+
+console.log(linearSearch([2, 55, 6, 77, 90, 6], 6));
