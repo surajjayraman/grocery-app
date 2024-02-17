c7ad78e6de16a600ebbf10393cc7ab86dfb1ba71
surajjayraman <suraj.jayraman@gmail.com>
2024-02-16 20:27:54 -0500

    Add higher order component example

---
+++ src/components/higherOrderComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function DogImages() {
+  return (
+    <div>
+      <p className="highlighted">Higher Order Component Pattern</p>
+      <h1>Dogs</h1>
+      <img
+        src="https://images.dog.ceo/breeds/terrier-irish/n02093991_100.jpg"
+        alt="A random dog"
+        width="300"
+      />
+    </div>
+  );
+}
