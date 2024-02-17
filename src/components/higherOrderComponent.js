83de6d5b489e88f637a4818a50a417ab0390f014
surajjayraman <suraj.jayraman@gmail.com>
2024-02-17 09:15:37 -0500

    Add func to fetch dog images

--- src/components/higherOrderComponent.js
+++ src/components/higherOrderComponent.js
@@ -1,11 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export default function DogImages() {
+  // fetch dog images
+  useEffect(() => {
+    async function fetchDogs() {
+      const { data } = await axios.get(
+        "https://dog.ceo/api/breed/labrador/images/random/4"
+      );
+      console.log(JSON.stringify(data));
+    }
+    fetchDogs();
+  }, []);
+
   return (
     <div>
       <p className="highlighted">Higher Order Component Pattern</p>
       <h1>Dogs</h1>
-      
     </div>
   );
 }
+
+// import { useState, useEffect } from "react";
+
+// export default function useDogImages() {
+//   const [dogs, setDogs] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchDogs() {
+//       const res = await fetch(
+//         "https://dog.ceo/api/breed/labrador/images/random/6"
+//       );
+//       const { message } = await res.json();
+//       setDogs(message);
+//     }
+
+//     fetchDogs();
+//   }, []);
+
+//   return dogs;
+// }
