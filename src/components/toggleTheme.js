48cc9d026234af8a4344fa6d764c65e2feed1739
surajjayraman <suraj.jayraman@gmail.com>
2024-02-19 16:18:48 -0500

    Update button styles toggle

--- src/components/toggleTheme.js
+++ src/components/toggleTheme.js
@@ -5,8 +5,14 @@ export default function ToggleTheme() {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <>
+      <p className="highlighted">Provider Pattern</p>
       Current theme: {theme}
-      <button onClick={toggleTheme}>Toggle Theme</button>
+      <button
+        style={{ "background-color": theme, color: "white" }}
+        onClick={toggleTheme}
+      >
+        Toggle Theme
+      </button>
     </>
   );
 }
--- src/providers/providerPattern.js
+++ src/providers/providerPattern.js
@@ -1,14 +1,14 @@
 import React, { createContext, useState } from "react";
 
-// Create a context for the provider
+// Create a context
 const ThemeContext = createContext("light");
 
 // Create a provider component
 const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("orange");
 
   const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    setTheme(theme === "orange" ? "purple" : "orange");
   };
 
   return (
