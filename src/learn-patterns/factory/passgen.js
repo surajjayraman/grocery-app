0e73e8d0daadc348effe35552b94fd0f916d9bf0
surajjayraman <suraj.jayraman@gmail.com>
2024-02-20 15:05:58 -0500

    Add passwd gen

---
+++ src/learn-patterns/factory/passgen.js
@@ -0,0 +1,12 @@
+function generatePassword(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  return password;
+}
+
+console.log(generatePassword(10)); // Example usage
