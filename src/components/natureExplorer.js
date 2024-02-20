4e0cc4a6b1cf0aa2a5387a7801b86939fca7bcda
surajjayraman <suraj.jayraman@gmail.com>
2024-02-20 14:37:52 -0500

    Add nature explorer app

---
+++ src/components/natureExplorer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LocationExplorer from "./LocationExplorer";
+
+function NatureExplorerApp() {
+  return (
+    <div>
+      <h1>Nature Explorer</h1>
+      <LocationExplorer />
+    </div>
+  );
+}
+
+export default NatureExplorerApp;
