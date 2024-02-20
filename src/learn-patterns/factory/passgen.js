3e34ade5238eba4bb8fd789ad3461989c6ade255
surajjayraman <suraj.jayraman@gmail.com>
2024-02-20 15:25:53 -0500

    Refactor passwd gen

--- src/learn-patterns/factory/passgen.js
+++ src/learn-patterns/factory/passgen.js
@@ -1,6 +1,17 @@
-function generatePassword(length) {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+function generatePassword(length, options = {}) {
+  const {
+    includeUppercase = true,
+    includeLowercase = true,
+    includeNumbers = true,
+    includeSpecialCharacters = true,
+  } = options;
+
+  let characters = "";
+  if (includeUppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercase) characters += "abcdefghijklmnopqrstuvwxyz";
+  if (includeNumbers) characters += "0123456789";
+  if (includeSpecialCharacters) characters += "!@#$%^&*()";
+
   let password = "";
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
@@ -9,4 +20,21 @@ function generatePassword(length) {
   return password;
 }
 
-console.log(generatePassword(10)); // Example usage
+function generateAndDisplayPassword() {
+  const length = parseInt(document.getElementById("length").value);
+  const includeUppercase = document.getElementById("includeUppercase").checked;
+  const includeLowercase = document.getElementById("includeLowercase").checked;
+  const includeNumbers = document.getElementById("includeNumbers").checked;
+  const includeSpecialCharacters = document.getElementById(
+    "includeSpecialCharacters"
+  ).checked;
+
+  const password = generatePassword(length, {
+    includeUppercase,
+    includeLowercase,
+    includeNumbers,
+    includeSpecialCharacters,
+  });
+
+  document.getElementById("password").textContent = password;
+}
